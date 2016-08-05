89840a17055b51e2fd8d7cdf28e01c8bc25ab6e9
magdalenaproebstl <magdalena.proebstl@gmail.com>
2016-08-05 07:51:03 +0200

    Initial Commit

---
+++ bachelorthesis/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bachelorthesis
+//
+//  Created by Magdalena Pröbstl on 05.08.16.
+//  Copyright © 2016 Magdalena Pröbstl. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
